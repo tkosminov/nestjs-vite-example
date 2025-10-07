c1aaa4b1c8a0c427011104b483d20249bd4d0ec6
t.kosminov <t.kosminov@oblakogroup.ru>
2025-10-07 16:25:18 +0300

    fix

--- vite.config.mjs
+++ vite.config.mjs
@@ -13,10 +13,10 @@ const src_path = resolve(root_path, 'src');
 
 const main_path = resolve(src_path, 'main.ts');
 const repl_nest_path = resolve(src_path, 'repl-nest/repl.ts');
-const repl_nest_custom = resolve(src_path, 'repl-custom/repl.ts');
+const repl_custom_path = resolve(src_path, 'repl-custom/repl.ts');
 const cli_path = resolve(src_path, 'typeorm/typeorm.cli.ts');
 
-const app_path = process.env.RUN_BUILD !== 'true' ? main_path : [main_path, repl_nest_path, repl_nest_custom, cli_path];
+const app_path = process.env.RUN_BUILD !== 'true' ? main_path : [main_path, repl_nest_path, repl_custom_path, cli_path];
 
 const config = defineConfig({
   server: {
